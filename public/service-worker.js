64e22485249fcb305948bfeb8515410a1170d226
Mike Sasiela <m5455y@gmail.com>
2021-03-22 21:21:41 -0500

    getting error site cannot be installed Page does not work offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,81 @@
-// install event handler
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('static').then( cache => {
-      return cache.addAll([
-        "/",
-        "./index.html",
-        "./css/styles.css",
-        "./js/index.js",
-        "./js/indexedDB.js",
-        "./icons/icon-192x192.png",
-        "./icons/icon-512x512.png",
-        "./manifest.webmanifest",
-        "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
-        "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
-      ]);
-    })
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/indexedDB.js",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png",
+  "./manifest.webmanifest",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+];
+
+// install
+self.addEventListener("install", function (evt) {
+  // pre cache all static assets
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
   );
-  console.log('Install');
+
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
   self.skipWaiting();
 });
 
-// retrieve assets from cache
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then( response => {
-      return response || fetch(event.request);
+// activate
+self.addEventListener("activate", function(evt) {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
+
+  self.clients.claim();
 });
+
+// fetch
+self.addEventListener("fetch", function(evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(evt.request.url, response.clone());
+            }
+
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(evt.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  evt.respondWith(fetch(evt.request).catch(()=>{
+    return caches.match(evt.request).then((response)=>{
+    if (response) {
+        return response
+    } else if (evt.request.headers.get("accept").includes("text/html")){
+        return caches.match("/")
+    }
+    })
+}))
+
+});
+
